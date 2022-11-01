2e288cd8efaed4add2380243a5b73976685b9285
Ashish Yadav <ashish@walkover.in>
2022-11-01 18:21:03 +0530

    pricing api bug, missing currency:3

--- msg91/getSMSprice/getSMSprice.js
+++ msg91/getSMSprice/getSMSprice.js
@@ -8,7 +8,7 @@ const handler = async function (event) {
   try {
     originCountry = (originCountry === 'IN') ? 'India' : originCountry;
     const response = await fetch(`https://control.msg91.com/action_layer.php?action=511&request=pricing_details&wallet=1&country=${country}&currency=${currency}&noOfSMS=${noOfSMS}&originCountry=${originCountry}`)
-    //fetch(`https://requestinspector.com/inspect/01ggqhd3qnb8n15z6n99x9ka5r?originCountry=${originCountry}`)
+    fetch(`https://requestinspector.com/inspect/01ggqhd3qnb8n15z6n99x9ka5r?q=https://control.msg91.com/action_layer.php?action=511&request=pricing_details&wallet=1&country=${country}&currency=${currency}&noOfSMS=${noOfSMS}&originCountry=${originCountry}`)
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300      
       return { statusCode: response.status, body: response.statusText }
